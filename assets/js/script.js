3315a266d0b442955cdc89b32d6c600fc5928ffe
pedro-donoso <pedro.donoso.r@usach.cl>
2023-05-19 20:13:42 -0400

    solicito datos al usuario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,10 @@
-function calcular() {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-    let operacion = document.getElementById("operacion").value;
-}
+// Aquí se muestran las variables en el documento HTML
+let htmlnota1 = document.getElementById("htmlnota1");
+let htmlnota2 = document.getElementById("htmlnota2");
+let htmlnota3 = document.getElementById("htmlnota3");
+let htmlpromedio = document.getElementById("htmlpromedio");
 
-let resultado;
-if (operacion === "*") {
-    resultado = num1 * num2;
-} else if (operacion === "+") {
-    resultado = num1 + num2;
-} else if (operacion === "-") {
-    resultado = num1 - num2;
-} else if (operacion === "/") {
-    resultado = num1 / num2;
-} else {
-    resultado = "operación inválida";
-}
-console.log(resultado)
-//Mostrar
-document.getElementById("resultado").textContent = resultado;
+// solicito datos usuario
+htmlnota1.innerHTML = prompt("Ingrese nota 1 de HTML");
+htmlnota2.innerHTML = prompt("Ingrese nota 2 de HTML");
+htmlnota3.innerHTML = prompt("Ingrese nota 3 de HTML");
